6042a695630abdeebca455b96680c2decdd5017c
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-20 19:54:52 +0000

    change config file, show house name and logo colour

--- config.js
+++ config.js
@@ -1,16 +1,58 @@
-let imageObject = {
-  "2": 'newhouse.jpg',
-  "4": 'sanderson.jpeg',
-  "8": 'dryden.png',
-  "16": 'kirkeby.png',
-  "32": 'wyatt.jpeg',
-  "64": '.jpg',
-  "128": '.jpg',
-  "256": '.jpg',
-  "512": '.jpg',
-  "1024": '.jpg',
-  "2048": '.jpg',
-  "4096":'.jpg',
-  "8192": '.jpg',
-  "16384":'.jpg'
+let colorsSizes =  {
+   "2": {
+    text:"New House",
+    color: "#312877"
+  },
+  "4": {
+    text:"Sanderson",
+    color: "#918abe"
+  },
+  "8": {
+    text:"Dryden",
+    color: "#e21e44"
+  },
+  "16": {
+    text:"Kirkeby",
+    color: "#006332"
+  },
+  "32": {
+    text:"Wyatt",
+    color: "#082b76"
+  },
+  "64": {
+    text:"Fisher",
+    color: "#15368b"
+  },
+  "128": {
+    text:"Crosby",
+    color: "#9b1f3a"
+  },
+  "256": {
+    text:"Grafton",
+    color: "#85bce3"
+  },
+  "512": {
+    text:"Sidney",
+    color: "#ac7aaf"
+  },
+  "1024": {
+    text:"St. A",
+    color: "#e33282"
+  },
+  "2048": {
+    text:"School",
+    color: "#e21e44"
+  },
+  "4096": {
+    text:"Bramston",
+    color: "#000000"
+  },
+  "8192": {
+    text:"Laundimer",
+    color: "#5d352a"
+  },
+  "16384": {
+    text:"LAXTON",
+    color: "#0079c0"
+  }
 }
